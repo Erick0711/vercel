cc10bfc26fe9d55b5b06e53d6024420a9fdd4dbe
Maicol Erick Arteaga Guzman <88513626+Erick0711@users.noreply.github.com>
2022-06-15 18:40:24 -0400

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,48 @@
 <?php
-phpinfo();
+    $destinatario = 'maicolarteaga0711@gmail.com';
+    // ESTO ES PARA EL CORREO AL QUE SE ENVIARA EL MENSAJE
+
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $correo = $_POST['correo'];
+    $asunto = $_POST['mensaje'];
+
+    echo $nombre;
+    echo $apellido;
+    echo $correo;
+    echo $asunto;
+    echo $destinatario;
+
+
+    $mensajeCompleto = $asunto ."\nAtentamente:". $nombre. " " . $apellido. $correo;
+    
+    mail($destinatario, "Contacto",$mensajeCompleto);
+
+    header("location:index.php");
 ?>
+
+<div class="container">
+
+        <form action="./index.php" method="POST">
+            <h1>CONTACTANOS</h1>
+            <div class="inputBox">
+            <input type="text" name="nombre" id="" required="required">
+            <span>Nombre</span>
+            </div>
+
+            <div class="inputBox">
+            <input type="text" name="apellido" id="" required="required">
+            <span>Apellido</span>
+
+            <div class="inputBox">
+            <input type="email" name="correo" id="" required="required">
+            <span>Correo</span>
+
+            <div class="inputBox">
+            <textarea name="mensaje" id="" cols="30" rows="10" required="required"></textarea>
+            <span>Asunto</span>
+
+            <input type="submit" value="Enviar" class="enviar">
+        </form>
+
+    </div>
