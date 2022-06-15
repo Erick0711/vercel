f10224040c15510e7abe551446560594ae328b4c
Maicol Erick Arteaga Guzman <88513626+Erick0711@users.noreply.github.com>
2022-06-15 18:31:56 -0400

    Update index.php

--- api/index.php
+++ api/index.php
@@ -7,20 +7,16 @@
     $correo = $_POST['correo'];
     $asunto = $_POST['mensaje'];
 
-    // $header = "ENVIADO DESDE EL PORTAFOLIO DE MAICOL ERICK";
-
-    $mensajeCompleto = $asunto ."\nAtentamente:". $nombre. " " . $apellido. $correo;
-    
-    mail($destinatario, "Contacto",$mensajeCompleto);
-
-    header("Location:index.html");
-    // echo "<script>alert('Correo Enviado')</script>";
-    // echo "<script>setTimeout(\"location.href='index.html'\", 1000)</script>";
+    echo $nombre;
+    echo $apellido;
+    echo $correo;
+    echo $asunto;
+    echo $destinatario;
 ?>
 
 <div class="container">
 
-        <form action="./correos.php" method="POST" class="contacto">
+        <form action="./index.php" method="POST">
             <h1>CONTACTANOS</h1>
             <div class="inputBox">
             <input type="text" name="nombre" id="" required="required">
